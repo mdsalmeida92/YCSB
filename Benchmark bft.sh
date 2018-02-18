d25340247d98632d137a52640068319f00e67459
Unknown <mds.almeida@campus.fct.unl.pt>
2018-02-18 00:15:36 +0000

    changed script for benchamark

--- Benchmark bft.sh
+++ Benchmark bft.sh
@@ -6,39 +6,58 @@ server_port="redis.port="$2
 
 serverType="Server:"$3
 redis_host=$4
+x=$5
+redis_port=6380
 
 echo $server_host
 echo $server_port
 
 
-for i in workloads/* ; 
+for i in workloads/* 
+do
 
 test=$(basename $i)
 echo $test"_"$serverType"_normal"
 
+for ((n=0;n<$x;n++))
+do 
+$redis_cli -h $redis_host -p $redis_port flushall
+((redis_port++))
+done
 
 
-$redis_cli -h $redis_host -p 6380 flushall
 
 ./bin/ycsb load redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/$test -p $server_host -p $server_port > outputLoad.txt
 
 ./bin/ycsb run redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/$test -p $server_host -p $server_port > BenchmarkOutput/$test/$test"_"$serverType"_normal".txt
 
-$redis_cli -h $redis_host -p 6380 flushall
+for ((n=0;n<$x;n++))
+do 
+$redis_cli -h $redis_host -p $redis_port flushall
+((redis_port++))
+done
 
 
 ./bin/ycsb load redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/$test -p $server_host -p $server_port -p "redis.encryption=E" > outputLoad.txt
 
 ./bin/ycsb run redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/$test -p $server_host -p $server_port -p "redis.encryption=E" > BenchmarkOutput/$test/$test"_"$serverType"_E".txt
 
-$redis_cli -h $redis_host -p 6380 flushall
+for ((n=0;n<$x;n++))
+do 
+$redis_cli -h $redis_host -p $redis_port flushall
+((redis_port++))
+done
 
 
 ./bin/ycsb load redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/$test -p $server_host -p $server_port -p "redis.encryption=EE" > outputLoad.txt
 
 ./bin/ycsb run redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/$test -p $server_host -p $server_port -p "redis.encryption=EE" > BenchmarkOutput/$test/$test"_"$serverType"_EE".txt
 
-$redis_cli -h $redis_host -p 6380 flushall
+for ((n=0;n<$x;n++))
+do 
+$redis_cli -h $redis_host -p $redis_port flushall
+((redis_port++))
+done
 
 done
 

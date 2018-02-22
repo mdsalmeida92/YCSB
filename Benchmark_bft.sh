6b297b522873abf0be888b2706d1f7127dcbd147
Unknown <mds.almeida@campus.fct.unl.pt>
2018-02-22 14:08:13 +0000

    Auto stash before merge of "master" and "origin/master"

--- Benchmark_bft.sh
+++ Benchmark_bft.sh
@@ -12,52 +12,61 @@ redis_port=6380
 echo $server_host
 echo $server_port
 
-
-for i in workloads/* 
-do
-
-test=$(basename $i)
-echo $test"_"$serverType"_normal"
-
 for ((n=0;n<$x;n++))
 do 
 $redis_cli -h $redis_host -p $redis_port flushall
 ((redis_port++))
 done
 
+./bin/ycsb load redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/all -p $server_host -p $server_port > outputLoad.txt
 
-
-./bin/ycsb load redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/$test -p $server_host -p $server_port > outputLoad.txt
+for i in workloads/* ; 
+do
+test=$(basename $i)
 
 ./bin/ycsb run redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/$test -p $server_host -p $server_port > BenchmarkOutput/$test/$test"_"$serverType"_normal".txt
 
+done
+
+
 for ((n=0;n<$x;n++))
 do 
 $redis_cli -h $redis_host -p $redis_port flushall
 ((redis_port++))
 done
 
+./bin/ycsb load redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/all -p $server_host -p $server_port -p "redis.encryption=E" > outputLoad.txt
 
-./bin/ycsb load redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/$test -p $server_host -p $server_port -p "redis.encryption=E" > outputLoad.txt
+for i in workloads/* ; 
+do
+test=$(basename $i)
+
 
 ./bin/ycsb run redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/$test -p $server_host -p $server_port -p "redis.encryption=E" > BenchmarkOutput/$test/$test"_"$serverType"_E".txt
 
+done
+
+
 for ((n=0;n<$x;n++))
 do 
 $redis_cli -h $redis_host -p $redis_port flushall
 ((redis_port++))
 done
 
+./bin/ycsb load redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/all -p $server_host -p $server_port -p "redis.encryption=EE" > outputLoad.txt
 
-./bin/ycsb load redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/$test -p $server_host -p $server_port -p "redis.encryption=EE" > outputLoad.txt
+for i in workloads/* ; 
+do
+test=$(basename $i)
+
 
 ./bin/ycsb run redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/$test -p $server_host -p $server_port -p "redis.encryption=EE" > BenchmarkOutput/$test/$test"_"$serverType"_EE".txt
 
+done
+
+
 for ((n=0;n<$x;n++))
 do 
 $redis_cli -h $redis_host -p $redis_port flushall
 ((redis_port++))
 done
-
-done
-

90dc654c5e39f7e99fba2e0116ae64c7e4336b39
Unknown <mds.almeida@campus.fct.unl.pt>
2018-02-24 22:25:30 +0000

    added scripts

---
+++ Benchmark_bft_EE.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+redis_cli=redis-cli
+server_host="redis.host="$1
+server_port="redis.port="$2
+
+serverType="Server:"$3
+redis_host=$4
+x=$5
+redis_port=6379
+
+echo $server_host
+echo $server_port
+
+for ((n=0;n<$x;n++))
+do 
+$redis_cli -h $redis_host -p $redis_port flushall
+((redis_port++))
+done
+
+./bin/ycsb load redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/all -p $server_host -p $server_port -p "redis.encryption=EE" > outputLoad.txt
+
+for i in workloads/* ; 
+do
+test=$(basename $i)
+
+
+./bin/ycsb run redis -jvm-args="-Djavax.net.ssl.trustStore=client.jks -Djavax.net.ssl.trustStorePassword=changeme" -s -P workloads/$test -p $server_host -p $server_port -p "redis.encryption=EE" > BenchmarkOutput/$test/$test"_"$serverType"_EE".txt
+
+done
+
+for ((n=0;n<$x;n++))
+do 
+$redis_cli -h $redis_host -p $redis_port flushall
+((redis_port++))
+done
